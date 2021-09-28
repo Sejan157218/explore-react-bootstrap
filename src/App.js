107e7edf0195473b849795f2d798aaae7bfd545a
sejan <sejan157218@gmail.com>
2021-09-28 16:10:49 +0600

    react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Spinner } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import News from './Component/News/News';
 
 function App() {
+  const [news,SetNews] =useState([])
+  useEffect(()=>{
+    fetch('https://newsapi.org/v2/everything?q=keyword&apiKey=839ad78f6a9641949bbde3b084af417f')
+      .then(res=>res.json())
+      .then(data=>SetNews(data.articles))
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container'>
+      {news.length===0 ? 
+        <Spinner animation="border" variant="success mt-5" />
+        : 
+        <Row xs={1} md={4} className="g-4">
+      {
+        news.map(nw=><News news={nw}></News>)
+      }
+      </Row>
+      }
     </div>
   );
 }
